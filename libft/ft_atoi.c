e02e423c363b85802ac3d740f3281b463307c3c7
Minato Abe <minato.cat.love@gmail.com>
2023-04-25 23:40:45 +0900

    error

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 10:57:14 by minabe            #+#    #+#             */
-/*   Updated: 2023/04/25 23:30:32 by minabe           ###   ########.fr       */
+/*   Updated: 2023/04/25 23:39:54 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ static void	error(void);
 
 int	ft_atoi(const char *str)
 {
-	int		sign;
-	size_t	i;
-	size_t	len;
-	int		nbr;
+	int			sign;
+	size_t		i;
+	size_t		len;
+	long long	nbr;
 
 	len = ft_strlen(str);
 	i = 0;
@@ -35,11 +35,11 @@ int	ft_atoi(const char *str)
 		if (!ft_isdigit(str[i]) || nbr < INT_MIN || INT_MAX < nbr)
 			error();
 		nbr = nbr * 10 + (str[i] - '0');
-		if ((nbr * 10 + (str[i] - '0')) / 10 != nbr)
-			error();
 		i++;
 	}
 	nbr *= sign;
+	if (nbr < INT_MIN || INT_MAX < nbr)
+		error();
 	return ((int)nbr);
 }
 
